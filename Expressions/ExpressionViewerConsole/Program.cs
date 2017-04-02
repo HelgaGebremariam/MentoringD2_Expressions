3cb6c6b780aeab67fb124cf0cda91c9bfcc1d104
helga <Helga.gebremariam@gmail.com>
2017-04-02 21:17:47 +0300

    Task 1 part 1

---
+++ Expressions/ExpressionViewerConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Expressions;
+using System.Linq.Expressions;
+
+namespace ExpressionViewerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Expression<Func<int, int>> source_exp = (c) => c - 1 + 2 * c;
+            var exchangeRules = new List<KeyValuePair<string, object>>();
+            exchangeRules.Add(new KeyValuePair<string, object>("c", 1));
+            ExpressionVisitorTransformer visitor = new ExpressionVisitorTransformer(exchangeRules);
+            visitor.VisitAndConvert(source_exp, "");
+
+        }
+    }
+}
---
+++ Expressions/Expressions/ExpressionVisitorTransformer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace Expressions
+{
+    public class ExpressionVisitorTransformer : ExpressionVisitor
+    {
+        private IEnumerable<KeyValuePair<string, object>> exchangeParameters;
+        public ExpressionVisitorTransformer() { }
+
+        public ExpressionVisitorTransformer(IEnumerable<KeyValuePair<string, object>> exchangeParameters) : base()
+        {
+            this.exchangeParameters = exchangeParameters;
+        }
+
+
+        private bool IsIntegerParameterType(Expression node)
+        {
+            return (node.NodeType == ExpressionType.Parameter && node.Type == typeof(int));
+        }
+
+        private bool IsConstantOneParameterType(Expression node)
+        {
+            var expr = node as ConstantExpression;
+            if(expr != null && expr.Type == typeof(int))
+            {
+                return (int)expr.Value == 1;
+            }
+            return false;
+        }
+
+        protected override Expression VisitParameter(ParameterExpression parameter)
+        {
+            var exchangeParameter = exchangeParameters.Where(p => p.Key == parameter.Name).FirstOrDefault();
+            if(exchangeParameter.Value == null)
+            {
+                return base.VisitParameter(parameter);
+            }
+            return Expression.Constant(exchangeParameter.Value);
+
+        }
+
+        //protected override Expression VisitBinary(BinaryExpression node)
+        //{
+        //    if (IsIntegerParameterType(node.Left) && IsConstantOneParameterType(node.Right))
+        //    {
+        //        if (node.NodeType == ExpressionType.Add)
+        //            return Expression.Increment(node.Left);
+        //        if (node.NodeType == ExpressionType.Subtract)
+        //            return Expression.Decrement(node.Left);
+        //    }
+        //    return base.Visit(node);
+        //}
+    }
+}
