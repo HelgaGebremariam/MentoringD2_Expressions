8ba84a215c867f180429a19f52bb68ae31f2bba0
Volha_Hebremaryiam <Volha_Hebremaryiam@epam.com>
2017-04-04 11:24:01 +0300

    Retrieve removed tests.

--- Expressions/Sample03/E3SProviderTests.cs
+++ Expressions/Sample03/E3SProviderTests.cs
@@ -10,11 +10,48 @@ namespace Sample03
 	[TestClass]
 	public class E3SProviderTests
 	{
+		[TestMethod]
+		public void WithoutProvider()
+		{
+			var client = new E3SQueryClient(ConfigurationManager.AppSettings["user"] , ConfigurationManager.AppSettings["password"]);
+            var res = client.SearchFTS<EmployeeEntity>(0, 1, "workstation:(EPRUIZHW0249)");
+
+            foreach (var emp in res)
+            {
+                Console.WriteLine("{0} {1}", emp.nativename, emp.startworkdate);
+            }
+        }
+
+		[TestMethod]
+		public void WithoutProviderNonGeneric()
+		{
+			var client = new E3SQueryClient(ConfigurationManager.AppSettings["user"], ConfigurationManager.AppSettings["password"]);
+            var res = client.SearchFTS(typeof(EmployeeEntity), 0, 10, "workstation:(EPRUIZHW0249)");
+
+            foreach (var emp in res.OfType<EmployeeEntity>())
+            {
+                Console.WriteLine("{0} {1}", emp.nativename, emp.startworkdate);
+            }
+        }
+
+
+		[TestMethod]
+		public void WithProvider()
+		{
+			var employees = new E3SEntitySet<EmployeeEntity>(ConfigurationManager.AppSettings["user"], ConfigurationManager.AppSettings["password"]);
+
+            foreach (var emp in employees.Where(e => e.workstation == "EPRUIZHW0249"))
+            {
+                Console.WriteLine("{0} {1}", emp.nativename, emp.startworkdate);
+            }
+
+
+        }
 
         [TestMethod]
         public void StartsWithTest()
         {
-			var employees = new E3SEntitySet<EmployeeEntity>(ConfigurationManager.AppSettings["user"], ConfigurationManager.AppSettings["password"]);
+            var employees = new E3SEntitySet<EmployeeEntity>(ConfigurationManager.AppSettings["user"], ConfigurationManager.AppSettings["password"]);
 
             foreach (var emp in employees.Where(e => e.workstation.StartsWith("EPRUIZHW0249")))
             {
